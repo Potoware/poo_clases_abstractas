98cb44537541e901be3e453363b49f8939941a37
Alejandro Potosi <alejandropotosi@outlook.com>
2021-09-01 18:13:38 -0500

    implementacion de clase abstracta validador con hijas NumeroValidador, NoNulo, RequeridoValidador

---
+++ src/com/potoware/pooclasesabstractas/form/validador/Validador.java
@@ -0,0 +1,10 @@
+package com.potoware.pooclasesabstractas.form.validador;
+
+abstract public class Validador {
+
+    protected String mensaje;
+
+    abstract public void setMensaje(String mensaje);
+    abstract public String getMensaje();
+    abstract public boolean esValido(String valor);
+}
